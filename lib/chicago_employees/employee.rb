b36a4cd7145bf926bc7e94440964ca91722ecb21
Jay Wengrow <jaywngrw@gmail.com>
2014-11-20 14:14:58 -0600

    fixed mappings to api

--- lib/chicago_employees/employee.rb
+++ lib/chicago_employees/employee.rb
@@ -3,10 +3,10 @@ module ChicagoEmployees
     attr_accessor :job_title, :department, :name, :salary
 
     def initialize(hash)
-      @job_title = hash["title"]
+      @job_title = hash["job_titles"]
       @department = hash["department"]
       @name = hash["name"]
-      @salary = hash["salary"]
+      @salary = hash["employee_annual_salary"]
     end
   end
 end
