52cecb1aa8f240c1bc75dd90574603e981739bce
Jay Wengrow <jaywngrw@gmail.com>
2014-11-20 14:41:00 -0600

    refactored out employee object generation

--- lib/chicago_employees.rb
+++ lib/chicago_employees.rb
@@ -5,20 +5,20 @@ require "chicago_employees/employee"
 
 module ChicagoEmployees
   def self.all
-    employees = []
     employee_hashes = Unirest.get("http://data.cityofchicago.org/resource/xzkq-xp2w.json").body
-    employee_hashes.each do |hash|
-      employees << Employee.new(hash)
-    end
-    employees
+    generate_employees(employee_hashes)
   end
 
   def self.where(hash)
-    employees = []
     url = "http://data.cityofchicago.org/resource/xzkq-xp2w.json"
     url += "?#{hash.keys.first.to_s}=#{hash.values.first}"
     employee_hashes = Unirest.get(url).body
-    employee_hashes.each do |hash|
+    generate_employees(employee_hashes)
+  end
+
+  def self.generate_employees(array)
+    employees = []
+    array.each do |hash|
       employees << Employee.new(hash)
     end
     employees
