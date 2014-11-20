0950f2d12aae9225c3ec512d2992582a80b8858d
Jay Wengrow <jaywngrw@gmail.com>
2014-11-20 14:09:22 -0600

    removing client

--- lib/chicago_employees.rb
+++ lib/chicago_employees.rb
@@ -1,7 +1,13 @@
 require "chicago_employees/version"
 require "chicago_employees/employee"
-require "chicago_employees/client"
 
 module ChicagoEmployees
-
+  def self.employees
+    employees = []
+    employee_hashes = Unirest.get("http://data.cityofchicago.org/resource/xzkq-xp2w.json").body
+    employee_hashes.each do |hash|
+      employees << Employee.new(hash)
+    end
+    employees
+  end
 end
